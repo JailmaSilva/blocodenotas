658323ab112a441bc8da38ce51a1fed7ff675366
Jailma Silva <mariajailma89@gmail.com>
2022-06-26 11:17:18 -0300

    Meus estudos JavaScript

---
+++ index.js
@@ -0,0 +1,2 @@
+// JavaScript Trybe:
+// Anota ai: Declarar uma váriavel é o mesmo que dizer 'criar uma váriavel' Em programação, declarar signica dizer ao computador para reservar um espaço na memória para armazenar um dado.
