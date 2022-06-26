22ae9f90023f0545cc82354ad557e531f00b13fe
Jailma Silva <mariajailma89@gmail.com>
2022-06-26 16:13:09 -0300

    Compilado história do JavaScript
    História do JavaScript

--- index.js
+++ index.js
@@ -1,2 +1,27 @@
 // JavaScript Trybe:
+let oQueEhProgramar = "O que é programar?";
+// O que é programar: dar ordens para que os computadores façam o que você quer que eles façam, de forma bem detalhada. Uma das linguagens que permitem escrever programas é o JavaScript, uma das mais predominantes no mercado.
+let historiaDoJs = "História do JavaScript";
+//Historia do JavaScript:O JavaScript foi criado em 1995, por Brendan Eich, com o objetivo de deixar as páginas da web mais interativas e dinâmicas, pois até então elas só apresentavam conteúdo estático. Sua criação foi um sucesso, e a linguagem efetivamente transformou a forma como usamos a web. Hoje, todos os navegadores têm suporte ao JavaScript e ele é o principal responsável pelas interações de pessoas usuárias com aplicações disponíveis na web.
+let papelDoJavaScript = "Papel do JavaScript no Dev web";
+//O papel do JavaScript no Desenvolvimento Web
+//Ao acessar uma página, você pode ver e interagir com diversos conteúdos, desde o botão play do YouTube até este texto que você está lendo agora. Todo código responsável por exibir algum conteúdo na tela do seu computador ou celular e que permita que você interaja com ele faz parte do que chamamos de front-end.
+
+//Para que uma página web funcione, é preciso ter recursos e dados para serem exibidos. Para isso, o back-end entra em ação: é ele que recebe as solicitações vindas do front-end, busca essas informações onde estiverem armazenadas e as devolve para serem exibidas na tela.
+
+//Imagine que você está em um restaurante e faz o pedido para o garçom, que leva seu pedido até a cozinha. A cozinha prepara a sua refeição e o garçom traz para você. Nesse caso, o garçom é o front-end. A cozinha, que recebe seu pedido, prepara e informa o garçom para entregar para você, é o back-end. 
+
+//Por exemplo, ao realizar seu login em uma rede social, o front-end é responsável por realizar a coleta dos dados e enviar ao back-end, que verifica se suas credenciais estão corretas e devolve informações do seu perfil para o front-end, que as carrega e exibe na tela. Atualmente o JavaScript é utilizado tanto para front-end quanto para back-end. Por isso, ao aprender a programar com essa linguagem, você está se preparando para trabalhar nesses dois contextos dentro do desenvolvimento web! 
+let nome = 'O potencial JavaScript no mercado de trabalho';
+//O potencial do JavaScript no mercado de trabalho
+//A versatilidade é algo que se destaca no JavaScript, que pode ser utilizado em laptops, smartphones, videogames, aplicativos, entre outros.
+
+//Muitas empresas de tecnologia têm ao menos parte do seu código escrito em JavaScript, incluindo gigantes da tecnologia como Netflix, Facebook, Google, Microsoft, LinkedIn e Uber.
+
+//O Facebook e o Google utilizam tanto o JavaScript que criaram ferramentas próprias para trabalhar melhor com a linguagem. Hoje essas ferramentas são públicas e estão disponíveis para que você as utilize para construir suas próprias páginas e aplicações.
+
+//E tem mais! Estudos apontam que, até 2025, serão criadas cerca de 797 mil vagas de T.I., e o Brasil forma apenas 53 mil pessoas por ano na área.
+let declararVariavél = "Declarar Variavel"
 // Anota ai: Declarar uma váriavel é o mesmo que dizer 'criar uma váriavel' Em programação, declarar signica dizer ao computador para reservar um espaço na memória para armazenar um dado.
+let name = "let";
+// "let" é utilizada para declarar uma variável. Ela é colocada antes do nome da variável para informar ao computador que ela deve ser criada.
