7f58492c8ad481e23190bb1925a863f292acedb1
Jailma Silva <mariajailma89@gmail.com>
2022-06-27 16:17:41 -0300

    Compilado
    Mais compilados dos meus estudos sobre js.

--- index.js
+++ index.js
@@ -31,7 +31,235 @@ let variaveisEhConstantes = "Variaveis e constantes";
 //O número da sua conta quando você acessa o sistema de um banco;
 //Uma mensagem enviada por um aplicativo podem ser encontradas guardadas em variáveis.
 //Entre essas e outras situações, as variáveis estão presentes no seu dia a dia e você nem percebe! 🥸
-let declararVariavél = "Declarar Variavel"
+let oQueSao = "variaveis"
+//O que são variáveis?
+//Variáveis são espaços na memória do computador que funcionam como caixas que possuem um nome e que podemos guardar alguma coisa dentro.
+
+//Para compreender melhor o funcionamento de uma variável, imagine o seguinte cenário:
+
+//Uma loja virtual vende presentes, como roupas, canecas e chaveiros. Para fazer uma entrega, o item comprado é colocado dentro de uma caixa, que possui uma etiqueta com a descrição do que tem dentro dela:
+//Vamos supor que é preciso fazer uma entrega de um produto para um cliente: temos a caixa que possui uma etiqueta de nome roupa e o produto a ser entregue, que é uma camiseta.
+
+//Podemos imaginar variáveis exatamente dessa mesma forma:
+
+//A caixa é a variável, que possui uma etiqueta representando o nome da variável: roupa;
+//O valor dessa variável é o que está dentro da caixa: camiseta.
+let declararVariavel = "Declarar Variavel";
 // Anota ai: Declarar uma váriavel é o mesmo que dizer 'criar uma váriavel' Em programação, declarar signica dizer ao computador para reservar um espaço na memória para armazenar um dado.
 let name = "let";
 // "let" é utilizada para declarar uma variável. Ela é colocada antes do nome da variável para informar ao computador que ela deve ser criada.
+//Vamos analisar o código:
+
+//let é uma palavra-chave utilizada para declarar uma variável. Ela é colocada antes do nome da variável para informar ao computador que ela deve ser criada;
+//roupa se refere ao nome da variável. Nesse caso, a caixa que representa a variável tem o nome roupa.
+//=, é o operador de atribuição. Atribui o valor 'camiseta' à variável roupa.
+//camiseta é o valor atribuído à variável, ou seja, o produto que é colocado dentro da caixa. Nesse caso, o conteúdo da caixa é uma 'camiseta'.
+//Por fim, o ponto e vírgula ; completa a estrutura. Esse sinal gráfico indica, na linguagem JavaScript, que você terminou de escrever aquela linha.
+//E se quisermos alterar o valor da variável, é possível? 🤔 A resposta é: sim! 😸
+
+//Voltando ao cenário da loja virtual, imagine que o comprador da camiseta quis fazer a troca do produto, antes mesmo do envio, e solicitou que a camiseta fosse trocada por um moletom. Sendo assim, o valor antigo 'camiseta' foi substituído por outro valor, o 'moletom'.
+
+//As variáveis guardam somente um único valor por vez, que pode ser trocado por outro. Toda vez que for preciso colocar um novo valor dentro da variável, o anterior será substituído.
+//Anota aí 📣: As variáveis guardam somente um único valor e, toda vez que a variável receber um novo valor, o antigo é substituído.
+
+//Veja como ficaria no código a atribuição do novo valor 'moletom' à variável 'roupa'
+//Percebeu que nesse caso não utilizamos a palavra let?
+
+//A palavra-chave é utilizada somente quando a variável é declarada, ou seja, quando você escreve ela no código pela primeira vez. Para reatribuir um valor à uma variável, a palavra-chave não deve ser repetida.
+
+//Legal, né? Agora a variável roupa passou a ter um novo valor: 'moletom'.
+let diferencaDePalavras = "var, const, let";
+//Diferenças entre as palavras-chave var, let e const
+//Existem três palavras-chave que você pode utilizar para declarar variáveis: var, let e const.
+
+//As palavras-chave let e const foram introduzidas à linguagem JavaScript em 2015. Antes disso só era possível usar a palavra-chave var.
+
+//Como o var é mais antigo, mas ainda há muito código escrito com ele, é importante conhecer essa palavra-chave. O let possui algumas vantagens em relação ao var e hoje em dia, é uma boa prática utilizar somente let e const para declarar variáveis. 😄
+
+//Vamos agora descobrir a diferença entre var, let e const:
+
+//O var tem praticamente as mesmas funcionalidades que o let:
+//Uma vez que você declarou uma variável com o var você também pode atribuir um novo valor a ela quando precisar:
+//A const tem uma diferença: uma característica dessa palavra-chave é que ela é uma constante. Isto significa que, uma vez que essa variável é declarada, o valor dela não poderá ser alterado.
+//Anota aí 📣: Com a palavra-chave let você poderá reatribuir valores à sua variável quantas vezes quiser. Com a palavra-chave const você só poderá atribuir o valor uma única vez.
+let tiposPrimitivos = "Tipos primitivos"
+//Por que isso é importante?
+//Assim como em qualquer linguagem de programação, o JavaScript lida com números, frases e outros tipos de valores que representam elementos da vida real, como, por exemplo, a idade e o nome de uma pessoa.
+
+//Para implementar códigos, é fundamental conhecer os tipos de dados que estão disponíveis e como trabalhar com cada um deles.
+
+//Tipos Primitivos no JavaScript
+//Ao utilizar computação para resolver problemas, é necessário representar informações da vida real, como números e textos. É para isso que os tipos primitivos servem, para que você consiga representar algo do mundo real em formato de código.
+
+//Em JavaScript, os tipos primitivos que você mais irá encontrar são os seguintes:
+
+//string: sequências de caracteres, como palavras, textos, senhas, etc.;
+//number: números, podendo ser positivos ou negativos, inteiros ou decimais;
+//boolean: pode ser representado apenas pelos valores verdadeiro (true) ou falso (false).
+//Existem também alguns tipos que representam dados que possuem valores nulos ou valores indefinidos:
+
+//null: é um valor atribuído como nulo, ou seja, é um valor vazio;
+//undefined: não possui um valor, ou seja, é um dado que não foi definido.
+//Testando o tipo de uma variável
+//Existe um operador no JavaScript chamado typeof, que identifica o tipo de uma variável!
+let string = "String";
+//Tipo primitivo string
+//Valores do tipo string são sequências de caracteres, escritos entre aspas simples (‘) ou duplas (“). Podem ser palavras, frases, textos inteiros, etc.
+//De olho na dica 👀 : Qualquer valor será do tipo string se for escrito entre aspas.
+let number = "number";
+//Tipo primitivo number
+//Em JavaScript, o tipo number pode representar duas categorias de números:
+
+//números inteiros, como 3;
+//números decimais, como 5.5.
+//Anota aí  : O separador decimal usado em linguagens de programação é o ponto, e não a vírgula.
+let boolean = 'boolean';
+//O tipo boolean, ou booleano em português, é um tipo que só pode assumir os valores verdadeiro ou falso. No contexto do JavaScript, o valor verdadeiro é true, e falso é false.
+
+ null = 'null';
+//Tipo primitivo null
+//Em certas situações, precisamos lidar no código com variáveis que ainda não sabemos o valor que ela vai receber. Para isso, existe o valor null, que faz com que a variável tenha um valor nulo.
+let undefined = 'undefined';
+//Tipo primitivo undefined
+//O tipo undefined é atribuído pelo JavaScript a variáveis que não possuem um valor ou que não foram declaradas, ou seja, são indefinidas. 
+let operadoresAritmeticos = "operadores aritmeticos"
+//Por que isso é importante?
+//Você acha que para trabalhar com desenvolvimento é necessário ter um dom na matemática e dominar todas as operações possíveis? Não é bem assim!
+
+//Uma pessoa programadora pode desenvolver lógicas inteiras sabendo apenas o fundamental da matemática, ou até mesmo não sabendo nada! Mesmo assim, você deve ter em mente que essa tão amada área do conhecimento tem grande importância para a programação.
+
+//Na programação, os operadores aritméticos são essenciais para que seja possível realizar operações para manipular dados com o objetivo de chegar ao resultado final desejado. Vamos usar o poder dos operadores a nosso favor?
+
+//Operações Aritméticas no JavaScript
+//Em JavaScript, a matemática não é muito diferente da dos livros. Operações, como adição ou subtração, são casualmente usadas para desenvolver algoritmos capazes de manipular valores, sejam eles números ou não.
+
+//Observe como podemos usar as quatro operações fundamentais (soma, subtração, multiplicação e divisão) em JavaScript:
+
+//De olho na dica 👀 : Use o console do navegador para praticar essas operações e analisar o que é retornado.
+
+//Os símbolos (+, -, *, e /) são o que chamamos de operadores aritméticos. Eles são utilizados para fazer operações com dois valores e produzir um novo valor único.
+
+//🤔 Opa! Então não é possível fazer operações com mais de dois valores?
+
+//Sim! É possível! Na programação, assim como na matemática, operações como 2 + 2 - 2 * 4 / 2 existem, e para que a operação retorne o resultado correto utilizamos o conceito de “ordem de precedência”, ou seja, calcula-se primeiro a multiplicação e a divisão, depois a soma e a subtração.
+
+//🤚 E se precisarmos calcular primeiro a operação de soma e de subtração para só depois calcular a operação de multiplicação e de divisão?
+
+//Para isso, podemos definir uma operação como prioridade usando os parênteses (). Dessa forma, pode-se mudar a operação para (2 + 2 - 2) * 4 / 2. Nesse caso, as operações de dentro dos parênteses (soma e subtração) serão calculadas primeiro, e só então o resultado é multiplicado por 4 e dividido por 2.
+
+//Dependendo dos operadores presentes em uma operação e do posicionamento deles, é possível chegar a diferentes resultados. 
+//Anota aí 📣 : na matemática existe um macete para saber a ordem de precedência das operações, o famoso ‘PEMDAS’:
+// Obs: se a multiplicação e a divisão, ou a soma e a subtração, estiverem na mesma operação, deve-se calcular da esquerda para a direita.
+
+//Outro operador que pode ser utilizado em JavaScript é o **, responsável por realizar operações de exponenciação. A exponenciação (ou potenciação) ocorre quando um dado número é multiplicado por ele mesmo, uma quantidade n de vezes
+//perações de módulo
+//A operação de módulo encontra o resto da divisão de um número por outro. É possível encontrar o módulo de uma divisão usando o operador de porcentagem (%). Por exemplo, o módulo de 4 divido por 2 é 0 (4 % 2 = 0), já o módulo de 4 divido por 3 é 1 (4 % 3 = 1).
+//Usando Operações Aritméticas para alterar valores no JavaScript
+//Em qualquer algoritmo que envolva manipulação de dados, provavelmente haverá uma operação aritmética, mesmo que esses dados não sejam numéricos. Uma operação aritmética é toda ação que manipula e modifica valores usando os operadores aritméticos.
+
+//Quando uma operação é realizada, o valor retornado por essa operação geralmente é armazenado em uma variável para que possa ser usada em alguma lógica do código. Por exemplo, no código abaixo, o valor retornado pela operação de soma é salvo na variável soma.
+let operadoresDeComparacao = 'operadores de comparacao';
+//Comparar valores
+//Quando desejamos obter o resultado da comparação entre dois valores, usamos pequenas expressões chamadas de Operadores de Comparação.
+
+//A estrutura de uma expressão desse tipo segue o seguinte formato ⬇️ :
+
+//Copiar
+//[valor] [operador de comparação] [valor]
+//Quando usamos esse formato de comparação, temos a possibilidade de tomar diferentes decisões ao longo do fluxo do programa. Os Operadores de Comparação não são usados somente no contexto de programação, provavelmente você já chegou a vê-los na Educação Básica. Confira a função de cada um deles abaixo:
+
+//=== para comparar estritamente a igualdade entre dois valores;
+//!== para comparar estritamente a diferença entre dois valores;
+//> para comparar se um valor é maior do que o outro;
+//< para comparar se um valor é menor do que o outro;
+//>= para comparar se um valor é maior ou igual do que o outro;
+//<= para comparar se um valor é menor ou igual do que o outro.
+//O processo de comparação ocorre com o acréscimo do Operador de Comparação entre dois valores. Sua função na estrutura é comparar o valor à esquerda com o valor à direita, assim como mostra o exemplo a seguir:
+
+//Copiar
+7 < 10 // 7 é menor que 10 (verdadeiro), então retorna true.
+7 > 10 // 7 é maior que 10 (falso), então retorna false. 
+//Vamos ver como funciona na prática?
+
+//Imagine que você precisa verificar a maioridade de uma pessoa. No Brasil, uma pessoa nessa condição tem 18 anos completos, ou seja, a pessoa que você vai verificar precisa estar dentro desse critério.
+
+//Talvez você tenha pensado na seguinte expressão:
+
+//“A idade da pessoa é maior ou igual a 18 anos?”
+//Primeiro, vamos supor que a pessoa que está sendo verificada tenha 17 anos. Convertendo simulação para código, temos:
+
+//Copiar
+//const idadeDaPessoa = 17;
+//const idadeMaioridade = 18;
+
+const verificaMaioridade = idadeDaPessoa >= idadeMaioridade; // aqui estamos verificando se idadeDaPessoa é maior ou igual que idadeMaioridade, o que retorna falso porque 17 não é maior e nem igual a 18
+	
+const verificaIdade = idadeDaPessoa === 17; // aqui estamos comparando se idadeDaPessoa é igual a 17, o que é verdade, retornando true
+//Nesse caso, temos o seguinte cenário:
+
+//verificaMaioridade recebe o valor false, pois é o resultado da comparação entre idadeDaPessoa e idadeMaioridade, ou seja, 17 não é maior e nem igual a 18.
+//verificaIdade recebe o valor true, pois é o resultado da comparação entre idadeDaPessoa e o número 17, ou seja, 17 é igual a 17.
+//Agora que já vimos quais são as atividades de comparação e como formular uma expressão - ou pergunta -, vamos nos aprofundar no funcionamento dos operadores. Bora lá? 💪
+let operadorIgualdade = '===';
+//O operador ===, chamado de “operador de igualdade estrita”, é utilizado para comparar dois valores. Veja o exemplo abaixo:
+
+
+const cidade = 'Belo Horizonte';
+
+const verificaCidade = cidade === 'Belo Horizonte';
+
+console.log(verificaCidade);
+//Vamos analisar o código:
+
+//A variável cidade está recebendo o valor ‘Belo Horizonte’. Esse valor é uma string com ordem específica de caracteres;
+//A variável verificaCidade está recebendo o resultado da comparação entre a variável cidade e a string ‘Belo Horizonte’, que retorna true, pois segue a mesma ordem de caracteres;
+//Se você executar o console.log(verificaCidade), verá que o resultado no console será true.
+//Anota aí 📣: O operador === só compara valores do mesmo tipo primitivo. Com isso, você deve comparar uma string somente com outra string e um number somente com outro number.
+let operadorDiferenteDe = '!==';
+//Agora, veremos sobre o operador !==. Aperte os cintos e bora rumo a mais conhecimentos! 🚀
+
+//É possível considerar o operador !== o oposto do operador ===, ou seja, !== é diferença estrita e === é igualdade estrita.
+
+//De olho na dica 👀 : Você pode chamar o operador !== de diferente de. Além disso, esse operador leva em consideração o tipo primitivo dos valores, assim como o operador ===.
+
+//Veja o exemplo:
+
+
+const nomeDoCachorro = 'Pipoca';
+
+const verificaNomeCachorro = nomeDoCachorro !== 'Pipoca'; // o valor da variável nomeDoCachorro é diferente de Pipoca?
+
+console.log(verificaNomeCachorro); // false
+//Vamos analisar:
+
+//A variável verificaNomeCachorro está recebendo o resultado da comparação entre o valor da variável nomeDoCachorro com o valor ‘Pipoca’. Com isso, temos a seguinte pergunta: O valor da variável nomeDoCachorro é diferente de ‘Pipoca’?
+//A resposta é: não, são valores iguais!
+
+//Portanto, o valor da variável verificaNomeCachorro é false, porque não atendeu à comparação de diferença, sacou?
+
+//Sendo assim, o que será impresso pelo console.log(verificaNomeCachorro); é false, pois os dois valores são iguais.
+
+//Anota aí 📣: O operador !== verifica se os valores são diferentes; desse modo, se um valor for diferente do outro, ele retorna true, e se for igual, retornará false.
+
+let operadorMaiorQueEMenorque = "< >";
+//chegou a hora de vermos sobre o operador >! Ele é utilizado quando queremos verificar se um valor é maior do que outro valor. Observe o exemplo a seguir, que compara a idade de uma pessoa com a classificação indicativa de um filme:
+
+
+const classificacaoIndicativa = 14;
+
+const idadeDaPessoa = 15;
+
+const verificaidade = idadeDaPessoa > classificacaoIndicativa;
+
+console.log(verificaidade);
+//Na expressão acima, você está comparando se a idadeDaPessoa é maior do que classificacaoIndicativa, e o resultado é true, pois a classificação indicativa do filme é de 14 anos, e a idadeDaPessoa é 15 anos, ou seja, 15 é maior que 14.
+
+//Caso a idadeDaPessoa seja maior do que a classificacaoIndicativa, o resultado atribuído a verificaIdade será true.
+//e olho na dica 👀 : Lembre-se de que é possível construir a expressão de acordo com sua lógica, desde que siga a estrutura [valor] [operador] [valor]!
+
+//Como vou conseguir lembrar que o operador > é maior e o < é menor? 🤔
+
+//Temos uma dica pra você: Observe que o símbolo de > parece o bico de um pássaro. Com isso você pode fazer a seguinte relação:
+
+//O pássaro maior > alimenta o pássaro menor;
+//O pássaro menor < é alimentado pelo maior.
